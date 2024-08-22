c718e063fa1c73449873993cd927eb9952087f7c
azamovamuxlisa <azamovamuxlisa04@gmail.com>
2024-08-22 10:40:57 +0500

    vs code

---
+++ 1-dars-vazifasi.js
@@ -0,0 +1,76 @@
+//1-masala
+let a=20;
+let P=4*a;
+console.log(P)
+//2-masala
+let b=30;
+let S=b*b;
+console.log(S)
+//3-masala
+let m=25;
+let n=15;
+let s=m*n;
+let p=2*(m+n);
+console.log(s)
+console.log(p)
+//4-masala
+const pi=3.14;
+let d=12;
+let L=pi*d;
+console.log(L)
+//5-masala
+let x=7;
+let V=x*x*x;
+let sirt=6*x*x;
+console.log(V)
+console.log(sirt)
+//6-masala
+let k=13;
+let y=25;
+let z=17;
+let v=k*y*z;
+let yuza=2*(k*y+k*z+y*z);
+console.log(v)
+console.log(yuza)
+//7-masala
+let r=53;
+const pii=3.14
+let uzunlik=2*pii*r;
+let yuzasi=pii*r*r;
+console.log(uzunlik)
+console.log(yuzasi)
+//8-masala
+let son1=39;
+let son2=43;
+let arifmetik=(son1+son2)/2;
+console.log(arifmetik)
+//9-masala
+let Son1=30;
+let Son2=34;
+let natija=64
+console.log(Math.sqrt(natija))
+//10-masala
+let a1=14
+let b1=36
+let s1=a1+b1
+let kopaytma=a1*b1
+let kvadrat1=a1*a1
+let kvadrat2=b1*b1
+console.log(s1)
+console.log(kopaytma)
+console.log(kvadrat1)
+console.log(kvadrat2)
+//11-masala
+let s3=34;
+let s4=13
+let s2=s3+s4
+let kopaytma1=s3*s4
+console.log(s2)
+console.log(kopaytma1)
+//12masala
+let a3=4
+let b3=3
+let c1=a3*a3+b3*b3
+console.log(Math.sqrt(c1))
+let p1=a3+b3+c1
+console.log(p1)
--- 2-dars.js
+++ 2-dars.js
@@ -1,1 +1,18 @@
-console.log('2-dars');
+// let x=10;
+// let y=20;
+// function add(a, b){
+//     return a+b;
+// }
+// let result=add(x,y);
+// console.log(result)
+// let a = 20, 
+//     b = 30;
+let a=20;
+let b=30;
+function divide(a, b) {
+    if(b == 0) {
+       throw 'Division by zero';
+    }
+    return b / a;
+}
+console.log(divide(a, b))
